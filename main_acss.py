4a67e58694f4cb2a7f5519caea19d9ddede8d65d
bbaattaahh <bbaattaahh@gmail.com>
2016-03-09 23:37:29 +0100

    first step, find the white coloumn

--- main_acss.py
+++ main_acss.py
@@ -1,7 +1,24 @@
 __author__ = 'Henrik'
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 
-ASP_PREZENTATION = np.zeros(100)
-print ASP_PREZENTATION.shape
+img = cv2.imread('images/test_img_1.jpg',0)
+
+
+temp = np.zeros(2560L)
+
+for x in range(0, 2560L):
+    temp[x] = sum(img[:,x])
+
+# plt.plot(temp)
+# plt.show()
+
+max_white = int(temp.argmax())
+
+my_img = img[:, (max_white-200):(max_white+100)]
+
+plt.imshow(my_img)
+
+print 1
