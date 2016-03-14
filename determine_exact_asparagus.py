fb06924758a9a411ed9048c6c6b7232c2259ffc5
bbaattaahh <bbaattaahh@gmail.com>
2016-03-14 16:54:49 +0100

    outsource the determination of the egsact asparagus, it will be the most complicated part of the code... I guass

---
+++ determine_exact_asparagus.py
@@ -0,0 +1,91 @@
+__author__ = 'Henrik'
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from asparagus import define_default_asparagus
+
+from find_the_asparagus import runningMean
+
+
+def determine_exact_asparagus(my_class):
+
+    first_asparagus = define_default_asparagus()
+
+    lower_white = np.array([get_the_lower_limit_of_white(my_class.sub_image)])
+    upper_white = np.array([255])
+
+
+    mask = cv2.inRange(my_class.sub_image, lower_white, upper_white)
+
+    kernel = np.ones((2,2),np.uint8)
+    dilation = cv2.dilate(mask,kernel,iterations = 1)
+
+
+    contours,hierarchy = cv2.findContours(dilation, 1, 2)
+
+    max_area_contur = get_max_area_contur(contours)
+
+
+    rect = cv2.minAreaRect(max_area_contur)
+    box = cv2.cv.BoxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(my_class.sub_image,[box],0,(0,0,255),10)
+
+    x,y,w,h = cv2.boundingRect(max_area_contur)
+    cv2.rectangle(my_class.sub_image,(x,y),(x+w,y+h),(0, 255, 0),10)
+
+
+    x_range1 = my_class.list_of_asparagus_subimages[0].top_left_corner_x
+    x_range2 = my_class.list_of_asparagus_subimages[0].top_left_corner_x + my_class.list_of_asparagus_subimages[0].width
+    y_range1 = my_class.list_of_asparagus_subimages[0].top_left_corner_y
+    y_range2 = my_class.list_of_asparagus_subimages[0].top_left_corner_y + my_class.list_of_asparagus_subimages[0].hight
+
+    my_class.original_picture[y_range1:y_range2, x_range1:x_range2] = my_class.sub_image
+
+    # cv2.imshow('frame',img)
+    # plt.imshow(img)
+
+
+    first_asparagus.hight_pixel = rect[1][1]
+    first_asparagus.width_pixel = rect[1][0]
+
+    my_class.add_asparagus(first_asparagus)
+
+    return None
+
+
+
+def get_the_lower_limit_of_white(img):
+    # TODO Not nice 15 parameter
+    hist = cv2.calcHist([img],[0],None,[256],[0,256])
+    moving_avarage_hist_signal = runningMean(hist, 15)
+
+    # plt.plot(hist)
+    # plt.plot(moving_avarage_hist_signal)
+    # plt.show()
+
+    max_place = moving_avarage_hist_signal.argmax()
+    next_min_place = max_place
+
+
+    while moving_avarage_hist_signal[next_min_place] > moving_avarage_hist_signal[next_min_place - 1]:
+        next_min_place = next_min_place - 1
+
+
+    return next_min_place
+
+
+
+def get_max_area_contur(conturs):
+    max_area_contur = conturs[0]
+    max_area = cv2.contourArea(max_area_contur)
+
+
+    for contur in conturs:
+        area = cv2.contourArea(contur)
+        if area > max_area :
+            max_area = area
+            max_area_contur = contur
+
+    return max_area_contur
--- main_acss.py
+++ main_acss.py
@@ -9,7 +9,9 @@ from whole_image import whole_image
 
 from find_the_asparagus import find_the_asparagus
 
-from determine_exact__asparagus import get_the_lower_limit_of_white
+from determine_exact_asparagus import get_the_lower_limit_of_white
+from determine_exact_asparagus import get_max_area_contur
+from determine_exact_asparagus import determine_exact_asparagus
 
 from calculate_millimeter_value_from_pixel_value import calculate_millimeter_value_from_pixel_value
 
@@ -21,26 +23,9 @@ PIXEL_MILLIMETER_RATIO = 110.0/1571.0
 
 
 
-def get_max_area_contur(conturs):
-    max_area_contur = conturs[0]
-    max_area = cv2.contourArea(max_area_contur)
+img = cv2.imread('images/test_img_3.jpg', 0)
+img_col = cv2.imread('images/test_img_3.jpg')
 
-
-    for contur in conturs:
-        area = cv2.contourArea(contur)
-        if area > max_area :
-            max_area = area
-            max_area_contur = contur
-
-    return max_area_contur
-
-
-
-img = cv2.imread('images/test_img_1.jpg', 0)
-img_col = cv2.imread('images/test_img_1.jpg')
-
-
-first_asparagus = define_default_asparagus()
 
 
 
@@ -56,45 +41,10 @@ my_class.add_asparagus_subimage(first_sub_img)
 my_class.calculate_sub_image(0)
 
 
-
-lower_white = np.array([get_the_lower_limit_of_white(my_class.sub_image)])
-upper_white = np.array([255])
-
-
-mask = cv2.inRange(my_class.sub_image, lower_white, upper_white)
-
-kernel = np.ones((2,2),np.uint8)
-dilation = cv2.dilate(mask,kernel,iterations = 1)
-
-
-contours,hierarchy = cv2.findContours(dilation, 1, 2)
-
-max_area_contur = get_max_area_contur(contours)
-
-
-rect = cv2.minAreaRect(max_area_contur)
-box = cv2.cv.BoxPoints(rect)
-box = np.int0(box)
-cv2.drawContours(my_class.sub_image,[box],0,(0,0,255),10)
-
-x,y,w,h = cv2.boundingRect(max_area_contur)
-cv2.rectangle(my_class.sub_image,(x,y),(x+w,y+h),(0, 255, 0),10)
-
-
-x_range1 = my_class.list_of_asparagus_subimages[0].top_left_corner_x
-x_range2 = my_class.list_of_asparagus_subimages[0].top_left_corner_x + my_class.list_of_asparagus_subimages[0].width
-y_range1 = my_class.list_of_asparagus_subimages[0].top_left_corner_y
-y_range2 = my_class.list_of_asparagus_subimages[0].top_left_corner_y + my_class.list_of_asparagus_subimages[0].hight
-
-img[y_range1:y_range2, x_range1:x_range2] = my_class.sub_image
-
-# cv2.imshow('frame',img)
-# plt.imshow(img)
-
-
-first_asparagus.hight_pixel = rect[1][1]
-first_asparagus.width_pixel = rect[1][0]
+determine_exact_asparagus(my_class)
+first_asparagus = my_class.list_of_asparaguses[0]
 calculate_millimeter_value_from_pixel_value(PIXEL_MILLIMETER_RATIO, first_asparagus)
+
 classify_the_asparagus(first_asparagus)
 
 my_class.add_asparagus(first_asparagus)
