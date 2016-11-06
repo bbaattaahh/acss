5a729c52952b58b4a8f77faa09be7750f8ead41b
bbaattaahh <bbaattaahh@gmail.com>
2016-11-06 15:22:26 +0100

    Extend Rectangle class with angle parameter

--- classes/rectangle.py
+++ classes/rectangle.py
@@ -1,16 +1,71 @@
+import numpy as np
+
+
 class Rectangle:
     def __init__(self,
                  top_left_x=0,
                  top_left_y=0,
                  width=0,
-                 high=0):
+                 high=0,
+                 angle=0):
 
         self.top_left_x = top_left_x
         self.top_left_y = top_left_y
         self.width = width
         self.high = high
-        self.vertices = [[top_left_x, top_left_y],
-                         [top_left_x + width, top_left_y],
-                         [top_left_x, top_left_y + high],
-                         [top_left_x + width, top_left_y + high]]
+        self.angle = angle
+        self.vertices = [self.top_left_corner(),
+                         self.top_right_corner(),
+                         self.bottom_right_corner(),
+                         self.bottom_left_corner()]
 
+    def top_left_corner(self):
+        return [self.top_left_x, self.top_left_y]
+
+    def top_right_corner(self):
+        top_left_corner = self.to_vector(self.top_left_x, self.top_left_y)
+        unrotated_vector = self.to_vector(self.width, 0)
+        rotMatrix = self.rotation_matrix(self.angle)
+        rotated_vector = np.dot(rotMatrix, unrotated_vector)
+        top_right_corner = top_left_corner + rotated_vector
+        top_right_corner_list = self.vector_to_list(top_right_corner)
+        return top_right_corner_list
+
+    def bottom_right_corner(self):
+        top_left_corner = self.to_vector(self.top_left_x, self.top_left_y)
+        unrotated_vector = self.to_vector(self.width, self.high)
+        rotMatrix = self.rotation_matrix(self.angle)
+        rotated_vector = np.dot(rotMatrix, unrotated_vector)
+        bottom_right_corner = top_left_corner + rotated_vector
+        bottom_right_corner_list = self.vector_to_list(bottom_right_corner)
+        return bottom_right_corner_list
+
+    def bottom_right_corner(self):
+        top_left_corner = self.to_vector(self.top_left_x, self.top_left_y)
+        unrotated_vector = self.to_vector(self.width, self.high)
+        rotMatrix = self.rotation_matrix(self.angle)
+        rotated_vector = np.dot(rotMatrix, unrotated_vector)
+        bottom_right_corner = top_left_corner + rotated_vector
+        bottom_right_corner_list = self.vector_to_list(bottom_right_corner)
+        return bottom_right_corner_list
+
+    def bottom_left_corner(self):
+        top_left_corner = self.to_vector(self.top_left_x, self.top_left_y)
+        unrotated_vector = self.to_vector(0, self.high)
+        rotMatrix = self.rotation_matrix(self.angle)
+        rotated_vector = np.dot(rotMatrix, unrotated_vector)
+        bottom_left_corner = top_left_corner + rotated_vector
+        bottom_left_corner_list = self.vector_to_list(bottom_left_corner)
+        return bottom_left_corner_list
+
+    def to_vector(self, x, y):
+        return np.array([[x],[y]])
+
+    def rotation_matrix(self, angle):
+        rad = np.deg2rad(angle)
+        rotMatrix = np.array([[np.cos(rad), -np.sin(rad)],
+                              [np.sin(rad), np.cos(rad)]])
+        return rotMatrix
+
+    def vector_to_list(self, vector):
+        return vector[0].tolist() + vector[1].tolist()
--- test/TestRectangle.py
+++ test/TestRectangle.py
@@ -1,31 +1,100 @@
 import unittest
 from Rectangle import Rectangle
+import math
+import numpy as np
 
 
 class TestRectangle(unittest.TestCase):
-    def test_vertices_calculation(self):
+    def test_vertices_calculation_when_angel_is_zero(self):
         # given
         x_top_left = 0
         y_top_left = 0
         width = 10
         high = 20
+        angle = 0
 
         expected_vertices = [[ 0,  0],
                              [10,  0],
-                             [ 0, 20],
-                             [10, 20]]
+                             [10, 20],
+                             [ 0, 20]]
 
         # when
         rectangle = Rectangle(x_top_left,
                               y_top_left,
                               width,
-                              high)
+                              high,
+                              angle)
 
         actual_vertices = rectangle.vertices
 
         # that
         self.assertEqual(actual_vertices, expected_vertices)
 
+    def test_vertices_calculation_when_angel_is_45(self):
+        # given
+        x_top_left = 0
+        y_top_left = 0
+        width = 10
+        high = 10
+        angle = 45
+
+        a = 5 * math.sqrt(2)
+
+        expected_vertices = [[0,   0],
+                             [a,   a],
+                             [0, 2*a],
+                             [-a,  a]]
+
+        # when
+        rectangle = Rectangle(x_top_left,
+                              y_top_left,
+                              width,
+                              high,
+                              angle)
+
+        actual_vertices = rectangle.vertices
+
+        # that
+        self.assertEqual((np.round(actual_vertices, 2) == np.round(expected_vertices, 2)).all(), True)
+
+    def test_to_vector(self):
+        # given
+        x = 6784
+        y = 6784
+
+        expected_vector = np.array([[6784], [6784]])
+
+        # when
+        rectangle = Rectangle()
+        actual_vector = rectangle.to_vector(x, y)
+
+        # that
+        self.assertEqual((actual_vector == expected_vector).all(), True)
+
+    def test_rotation_matrix(self):
+        # given
+        expected_rotation_matrix = np.array([[1, -0],
+                                             [0,  1]])
+
+        # when
+        angle = 0
+        rectangle = Rectangle()
+        actual_rotation_matrix = rectangle.rotation_matrix(angle)
+
+        # that
+        self.assertEqual((actual_rotation_matrix == expected_rotation_matrix).all(), True)
+
+    def test_vector_to_list(self):
+        # given
+        expected_list = [6784, 6784]
+
+        # when
+        vector = np.array([[6784], [6784]])
+        rectangle = Rectangle()
+        actual_list = rectangle.vector_to_list(vector)
+
+        # that
+        self.assertEqual(actual_list , expected_list)
 
 
 if __name__ == '__main__':
