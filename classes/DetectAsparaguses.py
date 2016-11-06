01a66ea4ccb7e0c60e520cbc16b7d07693ddd9c8
bbaattaahh <bbaattaahh@gmail.com>
2016-11-06 19:26:46 +0100

    Add asparagus_detection_candidates to DetectAsparagus class + test + test cascade files are oploaded as well

--- classes/DetectAsparaguses.py
+++ classes/DetectAsparaguses.py
@@ -3,57 +3,51 @@ import numpy as np
 import math
 
 import Rectangle
+import IsRectangleOnImage
 
 class DetectAsparaguses(object):
 
     def __init__(self,
                  image,
-                 cascade_classifier_file,
+                 cascade_file=None,
                  detection_scale=0.25,
                  swing_angle=45):
         self.image = image
-        self.cascade_classifier_file = cascade_classifier_file
+        self.cascade_file = cascade_file
         self.detection_scale=detection_scale
         self.swing_angle=swing_angle
+        self.image_detection_on=self.image_detection_on()
 
     def image_detection_on(self):
         grey_image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
         rescaled_image = cv2.resize(grey_image,
-                               None,
-                               fx=self.detection_scale,
-                               fy=self.detection_scale,
-                               interpolation=cv2.INTER_CUBIC)
+                                    None,
+                                    fx=self.detection_scale,
+                                    fy=self.detection_scale,
+                                    interpolation=cv2.INTER_CUBIC)
         return rescaled_image
 
-    def rectangles_contain_asparaguses(self):
+    def asparagus_detection_candidates(self):
         detections = dict()
 
-        for angle in range(-self.swing_angle, self.swing_angle):
-            rotated_image_detection_on = self.rotate_about_center(self.image_detection_on(), angle)
-            asparagus_cascade = cv2.CascadeClassifier(self.cascade_classifier_file)
-            detected_asparaguses = asparagus_cascade.detectMultiScale(rotated_image_detection_on, 4, 31)
-
-            for (x, y, w, h) in detected_asparaguses:
-                act_rectangle = Rectangle(x, y, w, h)
-
-                if self.is_rectangle_on_orignal_image(rectangle=act_rectangle,
-                                                      original_image=self.image_detection_on(),
-                                                      rotated_image=rotated_image_detection_on,
-                                                      angle=angle):
-                    detected_rectangles_in_this_angle = detections.get(angle, [])
-                    detections[angle] = detected_rectangles_in_this_angle.append([x, y, w, h])
+        for angle in range(-self.swing_angle, self.swing_angle+1):
+            rotated_image_detection_on = self.rotate_about_center(self.image_detection_on, angle)
+            asparagus_cascade = cv2.CascadeClassifier(self.cascade_file)
+            actual_asparagus_candidates = asparagus_cascade.detectMultiScale(rotated_image_detection_on, 4, 31)
+            if len(actual_asparagus_candidates) != 0:
+                detections[angle] = actual_asparagus_candidates.tolist()
 
         return detections
 
-    def rotate_about_center(src, angle, scale=1.):
+    def rotate_about_center(self, src, angle):
         w = src.shape[1]
         h = src.shape[0]
         rangle = np.deg2rad(angle)  # angle in radians
         # now calculate new image width and height
-        nw = (abs(np.sin(rangle) * h) + abs(np.cos(rangle) * w)) * scale
-        nh = (abs(np.cos(rangle) * h) + abs(np.sin(rangle) * w)) * scale
+        nw = abs(np.sin(rangle) * h) + abs(np.cos(rangle) * w)
+        nh = abs(np.cos(rangle) * h) + abs(np.sin(rangle) * w)
         # ask OpenCV for the rotation matrix
-        rot_mat = cv2.getRotationMatrix2D((nw * 0.5, nh * 0.5), angle, scale)
+        rot_mat = cv2.getRotationMatrix2D((nw * 0.5, nh * 0.5), angle, scale=1)
         # calculate the move from the old center to the new center combined
         # with the rotation
         rot_move = np.dot(rot_mat, np.array([(nw - w) * 0.5, (nh - h) * 0.5, 0]))
@@ -62,66 +56,56 @@ class DetectAsparaguses(object):
         rot_mat[0, 2] += rot_move[0]
         rot_mat[1, 2] += rot_move[1]
         return cv2.warpAffine(src, rot_mat, (int(math.ceil(nw)), int(math.ceil(nh))), flags=cv2.INTER_LANCZOS4)
-
-
-    def is_rectangle_on_orignal_image(self, original_image, rotated_image, rectangle, angle):
-        xy_limits = self.image_borders(original_image)
-
-        for vertex in rectangle.vertices:
-            original_vertex = self.calulate_original_coordinat_before_rotation(original_image,
-                                                                               rotated_image,
-                                                                               point=vertex,
-                                                                               angle=angle)
-
-            if not (xy_limits["x"][0] <= original_vertex[0] and original_vertex[0] <= xy_limits["x"][1]):
-                return False
-
-            if not (xy_limits["y"][0] <= original_vertex[1] and original_vertex[1] <= xy_limits["y"][1]):
-                return False
-
-        return True
-
-
-    def calulate_original_coordinat_before_rotation(original_image, rotated_image, point, angle):
-        point_numpy = np.array([[point[0]],[point[1]]])
-
-        w_original = original_image.shape[1]
-        h_original = original_image.shape[0]
-
-        w_rotated = rotated_image.shape[1]
-        h_rotated = rotated_image.shape[0]
-
-        # From top left coorner to centrum of image
-        centrum_vector = np.array([[-w_rotated / 2], [-h_rotated / 2]])
-
-        point_relative_to_centrum = point_numpy + centrum_vector
-
-        # Rotation back
-        theta = np.deg2rad(angle)
-        rotMatrix = np.array([[np.cos(theta), -np.sin(theta)],
-                              [np.sin(theta), np.cos(theta)]])
-        rotated_point_relative_to_centrum = np.dot(rotMatrix, point_relative_to_centrum)
-
-        # Centrum to top left coorner
-        top_left_centrum_point = rotated_point_relative_to_centrum - centrum_vector
-
-        # Compenyation with picture growth
-        w_growth_on_one_side = (w_rotated - w_original) / 2
-        h_growth_on_one_side = (h_rotated - h_original) / 2
-        compensation_vector = np.array([[w_growth_on_one_side], [h_growth_on_one_side]])
-
-        original_point = top_left_centrum_point - compensation_vector
-        original_point_list = original_point.tolist()
-        original_point_list_single_level = [original_point_list[0][0], original_point_list[1][0]]
-
-        return (original_point_list_single_level)
-
-
-    def image_borders(image):
-        x_min = 0
-        y_min = 0
-
-        x_max = image.shape[1]
-        y_max = image.shape[0]
-
-        return {"x": [x_min, x_max], "y": [y_min, y_max]}
+    #
+    #
+    # def is_rectangle_on_orignal_image(self, original_image, rotated_image, rectangle, angle):
+    #     xy_limits = self.image_borders(original_image)
+    #
+    #     for vertex in rectangle.vertices:
+    #         original_vertex = self.calulate_original_coordinat_before_rotation(original_image,
+    #                                                                            rotated_image,
+    #                                                                            point=vertex,
+    #                                                                            angle=angle)
+    #
+    #         if not (xy_limits["x"][0] <= original_vertex[0] and original_vertex[0] <= xy_limits["x"][1]):
+    #             return False
+    #
+    #         if not (xy_limits["y"][0] <= original_vertex[1] and original_vertex[1] <= xy_limits["y"][1]):
+    #             return False
+    #
+    #     return True
+    #
+    #
+    # def calulate_original_coordinat_before_rotation(original_image, rotated_image, point, angle):
+    #     point_numpy = np.array([[point[0]],[point[1]]])
+    #
+    #     w_original = original_image.shape[1]
+    #     h_original = original_image.shape[0]
+    #
+    #     w_rotated = rotated_image.shape[1]
+    #     h_rotated = rotated_image.shape[0]
+    #
+    #     # From top left coorner to centrum of image
+    #     centrum_vector = np.array([[-w_rotated / 2], [-h_rotated / 2]])
+    #
+    #     point_relative_to_centrum = point_numpy + centrum_vector
+    #
+    #     # Rotation back
+    #     theta = np.deg2rad(angle)
+    #     rotMatrix = np.array([[np.cos(theta), -np.sin(theta)],
+    #                           [np.sin(theta), np.cos(theta)]])
+    #     rotated_point_relative_to_centrum = np.dot(rotMatrix, point_relative_to_centrum)
+    #
+    #     # Centrum to top left coorner
+    #     top_left_centrum_point = rotated_point_relative_to_centrum - centrum_vector
+    #
+    #     # Compenyation with picture growth
+    #     w_growth_on_one_side = (w_rotated - w_original) / 2
+    #     h_growth_on_one_side = (h_rotated - h_original) / 2
+    #     compensation_vector = np.array([[w_growth_on_one_side], [h_growth_on_one_side]])
+    #
+    #     original_point = top_left_centrum_point - compensation_vector
+    #     original_point_list = original_point.tolist()
+    #     original_point_list_single_level = [original_point_list[0][0], original_point_list[1][0]]
+    #
+    #     return (original_point_list_single_level)
---
+++ test/TestDetectAsparaguses.py
@@ -0,0 +1,60 @@
+import unittest
+import cv2
+
+from DetectAsparaguses import DetectAsparaguses
+
+
+class TestDetectAsparaguses(unittest.TestCase):
+    def test_image_detection_on(self):
+        # given
+        image = cv2.imread("./images/DetectAsparaguses/test_image_detection_on_input.jpg")
+        detection_scale = 0.25
+
+        expected_output_image = cv2.imread("./images/DetectAsparaguses/test_image_detection_on_output.jpg",
+                                           cv2.CV_LOAD_IMAGE_GRAYSCALE)
+
+        # when
+        detect_asparaguses = DetectAsparaguses(image=image,
+                                               detection_scale=detection_scale)
+
+        # that
+        difference_because_of_jpg_compression = sum(sum(expected_output_image - detect_asparaguses.image_detection_on))
+        self.assertLess(difference_because_of_jpg_compression, 22000)
+
+    def test_asparagus_detection_candidates(self):
+        # given
+        image = cv2.imread("./images/DetectAsparaguses/test_asparagus_detection_candidates_input.jpg")
+        cascade_file = "./cascade_files/cascade.xml"
+        detection_scale = 0.25
+        swing_angle = 8
+
+        expected_candidates = dict()
+        expected_candidates[-8] = [[17, 41, 140, 25], [17, 63, 140, 25]]
+        expected_candidates[-7] = [[16, 40, 140, 25], [15, 63, 140, 25]]
+        expected_candidates[-6] = [[15, 39, 140, 25], [15, 62, 140, 25]]
+        expected_candidates[-5] = [[14, 61, 140, 25]]
+        expected_candidates[-4] = [[13, 60, 140, 25]]
+        expected_candidates[-3] = [[14, 58, 140, 25]]
+        expected_candidates[-2] = [[12, 58, 140, 25]]
+        expected_candidates[-1] = [[11, 56, 140, 25]]
+        expected_candidates[ 0] = [[ 9, 55, 140, 25]]
+        expected_candidates[ 1] = [[11, 56, 140, 25]]
+        expected_candidates[ 2] = [[12, 58, 140, 25]]
+        expected_candidates[ 3] = [[13, 60, 140, 25]]
+        expected_candidates[ 5] = [[15, 63, 140, 25]]
+        expected_candidates[ 6] = [[16, 65, 140, 25]]
+
+        # when
+        detect_asparaguses = DetectAsparaguses(image=image,
+                                               cascade_file=cascade_file,
+                                               detection_scale=detection_scale,
+                                               swing_angle=swing_angle)
+
+        actual_candidates = detect_asparaguses.asparagus_detection_candidates()
+
+        # that
+        self.assertEqual(actual_candidates, expected_candidates)
+
+
+if __name__ == '__main__':
+    unittest.main()
