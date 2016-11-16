baf7c0f8a7b0cbc4de2e24658a2ea08755b28b12
Henrik Bata <h.bata@mito.hu>
2016-11-16 17:02:04 +0100

    Camera calibration first working version + tests

---
+++ classes/CameraCalibration.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2
+
+
+class CameraCalibration:
+    def __init__(self,
+                 square_page_length_in_millimeter,
+                 image,
+                 chessboard_row_number=8,
+                 chessboard_column_number=8):
+
+        self.square_page_length_in_millimeter = square_page_length_in_millimeter
+        self.image = image
+        self.chessboard_row_number = chessboard_row_number
+        self.chessboard_column_number = chessboard_column_number
+        self.pixel_millimeter_ratio = self.pixel_millimeter_ratio()
+        self.millimeter_pixel_ratio = self.millimeter_pixel_ratio()
+        self.variance_square_page_length = self.variance_square_page_length()
+
+    def pixel_millimeter_ratio(self):
+        square_page_length = self.mean_square_page_length_in_pixel()
+        return square_page_length / self.square_page_length_in_millimeter
+
+    def millimeter_pixel_ratio(self):
+        square_page_length = self.mean_square_page_length_in_pixel()
+        return self.square_page_length_in_millimeter / square_page_length
+
+    def mean_square_page_length_in_pixel(self):
+        return np.mean(self.square_page_length_in_rows_in_pixel())
+
+    def variance_square_page_length(self):
+        return np.var(self.square_page_length_in_rows_in_pixel())
+
+    def square_page_length_in_rows_in_pixel(self):
+        corners = self.corner_points()
+        neighbour_distances = []
+        for column in range(0, self.chessboard_column_number - 1):
+            for row in range(0, self.chessboard_row_number - 2):
+                actual_point = corners[column * (self.chessboard_row_number - 1) + row][0]
+                left_neighbour = corners[column * (self.chessboard_row_number - 1) + row + 1][0]
+                actual_distance = np.linalg.norm(actual_point - left_neighbour)
+                neighbour_distances.append(actual_distance)
+
+        return neighbour_distances
+
+    def __gray_image(self):
+        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        return gray_image
+
+    def corner_points(self):
+        gray_image = self.__gray_image()
+        ret, corners = cv2.findChessboardCorners(gray_image,
+                                                 (self.chessboard_row_number - 1, self.chessboard_column_number - 1),
+                                                 None)
+        return corners
---
+++ test/TestCameraCalibration.py
@@ -0,0 +1,35 @@
+import unittest
+import cv2
+
+from CameraCalibration import CameraCalibration
+
+
+class TestCameraCalibration(unittest.TestCase):
+    def test_CameraCalibration_working(self):
+        # given
+        square_page_length_in_millimeter = 20.0
+        image = cv2.imread("./images/CameraCalibration/test_CameraCalibration_working.jpg")
+        chessboard_row_number = 8
+        chessboard_column_number = 8
+        camera_calibration = CameraCalibration(square_page_length_in_millimeter=square_page_length_in_millimeter,
+                                               image=image,
+                                               chessboard_row_number=chessboard_row_number,
+                                               chessboard_column_number=chessboard_column_number)
+
+        expected_pixel_millimeter_ratio = 3.5917140415736606
+        expected_millimeter_pixel_ratio = 0.27841860137670194
+        expected_variance_square_page_length = 1.8350508553641183
+
+        # when
+        actual_pixel_millimeter_ratio = camera_calibration.pixel_millimeter_ratio
+        actual_millimeter_pixel_ratio = camera_calibration.millimeter_pixel_ratio
+        actual_variance_square_page_length = camera_calibration.variance_square_page_length
+
+        # that
+        self.assertEqual(actual_pixel_millimeter_ratio, expected_pixel_millimeter_ratio)
+        self.assertEqual(actual_millimeter_pixel_ratio, expected_millimeter_pixel_ratio)
+        self.assertEqual(actual_variance_square_page_length, expected_variance_square_page_length)
+
+
+if __name__ == '__main__':
+    unittest.main()
