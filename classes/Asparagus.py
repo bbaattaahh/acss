b07764812edd62ce02d9bed65371b8e332b39df1
Henrik Bata <h.bata@mito.hu>
2016-11-17 15:03:59 +0100

    Asparagus class, it will deliver the info between the OneAspargusanalisis and the classification

---
+++ classes/Asparagus.py
@@ -0,0 +1,15 @@
+class Asparagus:
+    def __init__(self,
+                 length,
+                 thickness,
+                 head=False,
+                 purple_head=False,
+                 open_head=False,
+                 piper=False):
+
+        self.length = length
+        self.thickness = thickness
+        self.head = head
+        self.purple_head = purple_head
+        self.open_head = open_head
+        self.piper = piper
