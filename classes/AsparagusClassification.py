478fa0462c4aa16fcaf9770a43eeee1fe1fb9b6c
Henrik Bata <h.bata@mito.hu>
2016-11-21 10:49:45 +0100

    add test and some refactore to Asparagusclassification class, extend to all examined parameter of the aspragus (head, open head, purple head, piper, length, thickness)

--- classes/AsparagusClassification.py
+++ classes/AsparagusClassification.py
@@ -17,19 +17,24 @@ class AsparagusClassification:
         return "no class"
 
     def is_asparagus_fit_to_class(self, actual_class):
-        for actual_condition_key in self.classification_specification[actual_class]:
-            actual_condition = self.classification_specification[actual_class][actual_condition_key]
-            if not self.is_condition_full(actual_condition_key, actual_condition):
+        for actual_condition_name, actual_condition in self.classification_specification[actual_class].iteritems():
+            if not self.is_condition_full(actual_condition_name, actual_condition):
                 return False
         return True
 
-    def is_condition_full(self, actual_condition_key, actual_condition):
-        if actual_condition_key == "length":
-            return self.is_length_appropriate(actual_condition)
-        if actual_condition_key == "thickness":
-            return self.is_thickness_appropriate(actual_condition)
-        if actual_condition_key == "purple_head":
-            return self.is_purple_head_appropriate(actual_condition)
+    def is_condition_full(self, condition_name, condition):
+        if condition_name == "length":
+            return self.is_length_appropriate(condition)
+        if condition_name == "thickness":
+            return self.is_thickness_appropriate(condition)
+        if condition_name == "head":
+            return self.is_head_appropriate(condition)
+        if condition_name == "purple_head":
+            return self.is_purple_head_appropriate(condition)
+        if condition_name == "open_head":
+            return self.is_open_head_appropriate(condition)
+        if condition_name == "piper":
+            return self.is_piper_appropriate(condition)
 
     def is_length_appropriate(self, length_conditions):
         thickness_in_millimeter = self.asparagus.length * self.millimeter_pixel_ratio
@@ -41,14 +46,20 @@ class AsparagusClassification:
 
     def is_thickness_appropriate(self, thickness_conditions):
         thickness_in_millimeter = self.asparagus.thickness * self.millimeter_pixel_ratio
-
         if thickness_in_millimeter < thickness_conditions["lower_limit"]:
             return False
-
         if thickness_in_millimeter >= thickness_conditions["upper_limit"]:
             return False
-
         return True
+
+    def is_head_appropriate(self, expected_head):
+        return expected_head == self.asparagus.head
 
     def is_purple_head_appropriate(self, expected_purple_head):
         return expected_purple_head == self.asparagus.purple_head
+
+    def is_open_head_appropriate(self, expected_open_head):
+        return expected_open_head == self.asparagus.open_head
+
+    def is_piper_appropriate(self, expected_piper):
+        return expected_piper == self.asparagus.piper
--- test/TestAsparagusClassification.py
+++ test/TestAsparagusClassification.py
@@ -27,5 +27,49 @@ class TestAsparagusClassification(unittest.TestCase):
         # that
         self.assertEqual(actual_classification_result, expected_classification_result)
 
+    def test_asparagus_classification_no_head_no_class(self):
+        # given
+        asparagus = Asparagus(length=225,
+                              thickness=25,
+                              head=False,
+                              purple_head=False)
+        millimeter_pixel_ratio = 1
+        with open("./json_files/test_asparagus_classification_no_head_no_class.json") as json_data:
+            classification_specification = json.load(json_data)
+
+        expected_classification_result = "no class"
+
+        # when
+        asparagus_classification = AsparagusClassification(asparagus=asparagus,
+                                                           millimeter_pixel_ratio=millimeter_pixel_ratio,
+                                                           classification_specification=classification_specification)
+
+        actual_classification_result = asparagus_classification.classification_result
+
+        # that
+        self.assertEqual(actual_classification_result, expected_classification_result)
+
+    def test_asparagus_classification_minimum_condition_open_head(self):
+        # given
+        asparagus = Asparagus(length=225,
+                              thickness=25,
+                              open_head=True)
+        millimeter_pixel_ratio = 1
+        with open("./json_files/test_asparagus_classification_minimum_condition_open_head.json") as json_data:
+            classification_specification = json.load(json_data)
+
+        expected_classification_result = "open head"
+
+        # when
+        asparagus_classification = AsparagusClassification(asparagus=asparagus,
+                                                           millimeter_pixel_ratio=millimeter_pixel_ratio,
+                                                           classification_specification=classification_specification)
+
+        actual_classification_result = asparagus_classification.classification_result
+
+        # that
+        self.assertEqual(actual_classification_result, expected_classification_result)
+
+
 if __name__ == '__main__':
     unittest.main()
