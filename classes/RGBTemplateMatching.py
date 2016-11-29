6dd861d73c557fc66825cfdb6aaea823425f7f7f
bbaattaahh <bbaattaahh@gmail.com>
2016-11-29 13:41:35 +0100

    bucket detection refactoring

--- classes/RGBTemplateMatching.py
+++ classes/RGBTemplateMatching.py
@@ -32,19 +32,37 @@ class RGBTemplateMatching:
         top_left_corners = []
 
         y_coordinate = int(np.mean(self.locations[0]))
+        x_coordinates = self.get_x_coordinates()
+
+        for x_coordinate in x_coordinates:
+            top_left_corners.append([x_coordinate, y_coordinate])
+
+        return top_left_corners
+
+    def get_x_coordinates(self):
+        x_coordinates_to_one_match = []
 
         sorted_x_coordinates = np.sort(self.locations[1])
         numeric_derivation_of_sorted_x_coordinates = np.diff(sorted_x_coordinates)
         fractures = np.where(numeric_derivation_of_sorted_x_coordinates >= self.width)
-        fractures = np.asarray(fractures)[0]
-        fractures = fractures.tolist()
-        fractures = map(lambda x: x + 1, fractures)
+        fractures = self.tuple_numpy_array_to_list(fractures)
+        fractures = self.numerical_derivation_position_correction(fractures)
 
         fractures = [0] + fractures + [len(sorted_x_coordinates)]
 
         for fracture_index in range(len(fractures)-1):
             one_group_x_coordinates = sorted_x_coordinates[fractures[fracture_index]:fractures[fracture_index+1]]
-            x_coordinate = int(np.mean(one_group_x_coordinates))
-            top_left_corners.append([x_coordinate,y_coordinate])
+            mean_x_coordinate = int(np.mean(one_group_x_coordinates))
+            x_coordinates_to_one_match.append(mean_x_coordinate)
 
-        return top_left_corners
+        return x_coordinates_to_one_match
+
+    @staticmethod
+    def tuple_numpy_array_to_list(tuple_numpy_array):
+        np_array = np.asarray(tuple_numpy_array)[0]
+        python_list = np_array.tolist()
+        return python_list
+
+    @staticmethod
+    def numerical_derivation_position_correction(series):
+        return map(lambda x: x + 1, series)
--- test/TestRGBTemplateMatching.py
+++ test/TestRGBTemplateMatching.py
@@ -26,6 +26,49 @@ class TestRGBTemplateMatching(unittest.TestCase):
         # that
         self.assertEqual(actual_rectangle_top_left_vertices, expected_rectangle_top_left_vertices)
 
+    def test_get_x_coordinates_working(self):
+        # given
+        rgb_image = cv2.imread("./images/RGBTemplateMatching/test_get_x_coordinates_working_rgb_image.jpg")
+        rgb_template = cv2.imread("./images/RGBTemplateMatching/test_get_x_coordinates_working_rgb_template.jpg")
+        threshold = 2.3
+        cv2_method = cv2.TM_CCOEFF_NORMED
+        rgb_template_matching = RGBTemplateMatching(rgb_image=rgb_image,
+                                                    rgb_template=rgb_template,
+                                                    threshold=threshold,
+                                                    cv2_method=cv2_method)
+
+        expected_x_coordinates = [729, 1507]
+
+        # when
+        actual_x_coordinates = rgb_template_matching.get_x_coordinates()
+
+        # that
+        self.assertEqual(actual_x_coordinates, expected_x_coordinates)
+
+    def test_tuple_numpy_array_to_list_working(self):
+        # given
+        tuple_numpy_array = (np.array([6784, 1234]),)
+
+        expected_list = [6784, 1234]
+
+        # when
+        actual_list = RGBTemplateMatching.tuple_numpy_array_to_list(tuple_numpy_array)
+
+        # that
+        self.assertEqual(actual_list, expected_list)
+
+    def test_numerical_derivation_position_correction_working(self):
+        # given
+        positions = [10, 6784]
+
+        expected_corrected_positions = [11, 6785]
+
+        # when
+        actual_corrected_positions = RGBTemplateMatching.numerical_derivation_position_correction(positions)
+
+        # that
+        self.assertEqual(actual_corrected_positions, expected_corrected_positions)
+
 
 if __name__ == '__main__':
     unittest.main()
