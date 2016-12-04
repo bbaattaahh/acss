7461fa3250da28acf4f835e7712daa30abdeb88c
bbaattaahh <bbaattaahh@gmail.com>
2016-12-04 19:10:58 +0100

    Define Buckets

---
+++ classes/Bucket.py
@@ -0,0 +1,9 @@
+class Bucket:
+    def __init__(self,
+                 start="Unknown",
+                 end="Unknown",
+                 bucket_number="Unknown"):
+
+        self.start = start
+        self.end = end
+        self.bucket_number = bucket_number
