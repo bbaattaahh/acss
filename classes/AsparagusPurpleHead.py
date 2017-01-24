b9becc9184be7bc8d44e2524febaaa63d5191983
bbaattaahh <bbaattaahh@gmail.com>
2017-01-24 22:10:02 +0100

    AsparagusPurpleHead, second try

--- classes/AsparagusPurpleHead.py
+++ classes/AsparagusPurpleHead.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
+from KeepNLargestAreaContours import KeepNLargestAreaContours
+
 
 class AsparagusPurpleHead:
-
     def __init__(self,
                  image,
                  bounder=([200, 127, 200], [255, 200, 255])):
-
         self.image = image
         self.bounder = bounder
 
@@ -24,3 +24,39 @@ class AsparagusPurpleHead:
         output = cv2.bitwise_and(self.image, self.image, mask=mask)
 
         return output
+
+    @property
+    def green_layer(self):
+        green_layer = self.asparagus_with_masked_background[:, :, 1]
+        return green_layer
+
+    @property
+    def asparagus_with_masked_background(self):
+        asparagus_with_masked_background = cv2.bitwise_and(self.image, self.image, mask=self.asparagus_mask)
+        return asparagus_with_masked_background
+
+    @property
+    def asparagus_mask(self):
+        gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        ret, th = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        th = self.set_img_frame_black(th)
+        kernel = np.ones((5, 5), np.uint8)
+        th = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel)
+        keep_largest_area_contour = KeepNLargestAreaContours(image=th, kept_contour_number=1, invert_flag=False)
+        one_contour_image = keep_largest_area_contour.kept_n_largest_area_contours_image
+
+        closed_asparagus_contour = cv2.morphologyEx(one_contour_image, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+        return closed_asparagus_contour
+
+    @staticmethod
+    def set_img_frame_black(image):
+        height, width = image.shape
+
+        image[:, 0] = 0
+        image[:, width - 1] = 0
+        image[0, :] = 0
+        image[height - 1, :] = 0
+
+        return image
--- test/TestAsparagusPurpleHead.py
+++ test/TestAsparagusPurpleHead.py
@@ -16,11 +16,57 @@ class TestAsparagusPurpleHead(unittest.TestCase):
 
         # when
         actual_color_filtered_image = asparagus_purple_head.color_filtered_image
-        cv2.imwrite("./images/AsparagusPurpleHead/test_color_filtered_image_working_output.png",
-                    actual_color_filtered_image)
 
         # that
         self.assertEqual(np.array_equal(actual_color_filtered_image, expected_color_filtered_image), True)
+
+    def test_green_layer_working(self):
+        # given
+        image = cv2.imread("./images/AsparagusPurpleHead/test_green_layer_working_input.png")
+        asparagus_purple_head = AsparagusPurpleHead(image=image)
+
+        expected_green_layer \
+            = cv2.imread("./images/AsparagusPurpleHead/test_green_layer_working_output.png",
+                         cv2.CV_LOAD_IMAGE_GRAYSCALE)
+
+        # when
+        actual_green_layer = asparagus_purple_head.green_layer
+
+        # that
+        self.assertEqual(np.array_equal(actual_green_layer, expected_green_layer), True)
+
+    def test_asparagus_with_masked_background_working(self):
+        # given
+        image = cv2.imread("./images/AsparagusPurpleHead/test_asparagus_with_masked_background_working_input.png")
+        asparagus_purple_head = AsparagusPurpleHead(image=image)
+
+        expected_asparagus_with_masked_background \
+            = cv2.imread("./images/AsparagusPurpleHead/test_asparagus_with_masked_background_working_output.png")
+
+        # when
+        actual_asparagus_with_masked_background = asparagus_purple_head.asparagus_with_masked_background
+
+        # that
+        self.assertEqual(np.array_equal(actual_asparagus_with_masked_background,
+                                        expected_asparagus_with_masked_background),
+                         True)
+
+    def test_asparagus_mask_working(self):
+        # given
+        image = cv2.imread("./images/AsparagusPurpleHead/test_asparagus_mask_working_input.png")
+        asparagus_purple_head = AsparagusPurpleHead(image=image)
+
+        expected_asparagus_contour \
+            = cv2.imread("./images/AsparagusPurpleHead/test_asparagus_mask_output_working.png",
+                         cv2.IMREAD_GRAYSCALE)
+
+        # when
+        actual_asparagus_contour = asparagus_purple_head.asparagus_mask
+        cv2.imwrite("./images/AsparagusPurpleHead/test_asparagus_mask_output_working.png",
+                    actual_asparagus_contour)
+
+        # that
+        self.assertEqual(np.array_equal(actual_asparagus_contour, expected_asparagus_contour), True)
 
 
 if __name__ == '__main__':
