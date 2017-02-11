a9ef9d4d46bb58bac51b106d26d15bca0b3bf201
bbaattaahh <bbaattaahh@gmail.com>
2017-02-11 17:48:27 +0100

    head_sample_corrector working, input ideo file names specify the class names

--- auxiliary_scripts/head_sample_collector/head_sample_collector.py
+++ auxiliary_scripts/head_sample_collector/head_sample_collector.py
@@ -1,61 +1,61 @@
 from moviepy.editor import *
 import cv2
 import json
+import os
 
 from DetectAsparaguses import DetectAsparaguses
 from OneAsparagusAnalysis import OneAsparagusAnalysis
 from AsparagusHeadImage import AsparagusHeadImage
 
-# PARAMS
+
 with open('config/config-local.json') as json_data:
     config = json.load(json_data)
 
 
-STRECH_PARAMETER = 0.25
-ROTATION_ANGLE = 15
-MIN_AREA = 1
+def get_video_file_names(config):
+    video_file_names = []
+    for actual_video_file_name in config["head_classification"]["videos"]:
+        actual_full_name = config["dropbox_folder_path"] + actual_video_file_name
+        video_file_names.append(actual_full_name)
 
-CASCADE_FILE = config["dropbox_folder_path"] + '/haar_cascade/data_whole_rotate/cascade.xml'
-VIDEO_FILE = config["dropbox_folder_path"] + '/videos/live_test_2.avi'
-RESULT_IMAGE_FOLDER = config["dropbox_folder_path"] + '/head_sample_collector/'
-
-
-def is_asparagus(w, h, min_area):
-    if h*w > min_area:
-        return True
-
-    return False
-
-
-# this is the cascade we just made. Call what you want
-asparagus_cascade = cv2.CascadeClassifier(CASCADE_FILE)
-
-clip = VideoFileClip(VIDEO_FILE)
+    return video_file_names
 
 i = 0
-for x in clip.iter_frames():
 
-    act_frame_rgb = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+video_files = get_video_file_names(config)
+for actual_video_file in video_files:
+    clip = VideoFileClip(actual_video_file)
+    actual_basename = os.path.basename(actual_video_file)
+    actual_class_name = os.path.splitext(actual_basename)[0]
 
-    detect_asparaguses = DetectAsparaguses(image = act_frame_rgb,
-                                           cascade_file = CASCADE_FILE,
-                                           detection_resolution=(120, 160),
-                                           swing_angle=15)
+    for x in clip.iter_frames():
 
-    if len(detect_asparaguses.data_to_analysis_one_asparagus_images) <> 0:
+        act_frame_rgb = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
 
-        act_detection = OneAsparagusAnalysis(detect_asparaguses.data_to_analysis_one_asparagus_images[0]).asparagus_in_smallest_enclosing_box
-        act_top_part = AsparagusHeadImage(act_detection,
-                                          top_part_to_keep_ratio=0.15,
-                                          output_resolution=(50, 50)).resized_top_part
+        data_to_analysis_one_asparagus_images = DetectAsparaguses(image = act_frame_rgb,
+                                               cascade_file = config["dropbox_folder_path"] + config["haar_cascade_file"],
+                                               detection_resolution=(120, 160),
+                                               swing_angle=15).data_to_analysis_one_asparagus_images
 
-        i += 1
-        file_name = RESULT_IMAGE_FOLDER + str(i) + ".jpg"
-        cv2.imwrite(file_name, act_top_part)
+        if len(data_to_analysis_one_asparagus_images) <> 0:
+            for data_to_analysis_one_asparagus_image in data_to_analysis_one_asparagus_images:
+                act_detection = OneAsparagusAnalysis(data_to_analysis_one_asparagus_image).asparagus_in_smallest_enclosing_box
+                act_top_part = AsparagusHeadImage(act_detection,
+                                                  top_part_to_keep_ratio=0.15,
+                                                  output_resolution=(50, 50)).top_part
 
-        # cv2.imshow('img', act_detection)
-        # k = cv2.waitKey(30) & 0xff
-        # if k == 27:
-        #     break
+                i += 1
+                file_name = config["dropbox_folder_path"]\
+                            + config["head_classification"]["head_pictures_folder"] \
+                            + actual_class_name\
+                            + "_"\
+                            + str(i)\
+                            + ".jpg"
+                cv2.imwrite(file_name, act_top_part)
+
+                # cv2.imshow('img', act_detection)
+                # k = cv2.waitKey(30) & 0xff
+                # if k == 27:
+                #     break
 
 cv2.destroyAllWindows()
