bad94b3c3d04d1a0bc3cb23317eb67124986e51a
bbaattaahh <bbaattaahh@gmail.com>
2017-03-29 22:43:16 +0200

    Bugfix, Extend SnipFromImage, now it is handle the cases when we want to snip from out of the image

--- classes/SnipFromImage.py
+++ classes/SnipFromImage.py
@@ -8,17 +8,32 @@ class SnipFromImage(object):
                  h):
 
         self.image = image
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
+        if x < 0:
+            self.x1 = 0
+        else:
+            self.x1 = x
+
+        if y < 0:
+            self.y1 = 0
+        else:
+            self.y1 = y
+
+        if x + w > image.shape[1]:
+            self.x2 = image.shape[1]
+        else:
+            self.x2 = x + w
+
+        if y + h > image.shape[0]:
+            self.y2 = image.shape[0]
+        else:
+            self.y2 = y + h
 
     @property
     def snipped_image(self):
         # gray image
         if len(self.image.shape) == 2:
-            return self.image[self.y: self.y + self.h, self.x: self.x + self.w]
+            return self.image[self.y1 : self.y2, self.x1 : self.x2]
 
         # rgb image
         if len(self.image.shape) == 3:
-            return self.image[self.y: self.y + self.h, self.x: self.x + self.w, :]
+            return self.image[self.y1: self.y2, self.x1 : self.x2, :]
