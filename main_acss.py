cbb3f345003bab49366c56a1bf940e049230b920
bbaattaahh <bbaattaahh@gmail.com>
2017-04-05 14:39:02 +0200

    main_acss first full version

--- main_acss.py
+++ main_acss.py
@@ -3,9 +3,11 @@ import json
 
 from BucketsDetector import BucketsDetector
 from AsparagusesDetector import AsparagusesDetector
+from OneAsparagusAnalyzer import OneAsparagusAnalyzer
 from AsparagusClassifier import AsparagusClassifier
 from AsparagusHeadClassifier import AsparagusHeadClassifier
-
+from MergeBucketsAndAsparagusesPositions import MergeBucketsAndAsparagusPositions
+from DisplayClassification import DisplayClassification
 
 __author__ = 'Henrik'
 
@@ -25,6 +27,7 @@ asparaguses_detector = AsparagusesDetector(config["asparaguses_detector"]["casca
                                            tuple(config["asparaguses_detector"]["detection_resolution"]),
                                            config["asparaguses_detector"]["swing_angle"])
 
+
 asparagus_classifier = AsparagusClassifier(config["asparagus_classifier"]["millimeter_pixel_ratio"],
                                            config["asparagus_classifier"]["classification_specification_file"])
 
@@ -35,6 +38,10 @@ asparagus_head_classifier = \
                             config["asparagus_head_classifier"]["top_part_to_keep_ratio"],
                             tuple(config["asparagus_head_classifier"]["head_classification_resolution"]))
 
+one_asparagus_analyzer = OneAsparagusAnalyzer(asparagus_head_classifier)
+
+displayer = DisplayClassification()
+
 cap = cv2.VideoCapture(0)
 cap.set(3, config["web_camera_distribution"][0])
 cap.set(4, config["web_camera_distribution"][1])
@@ -43,6 +50,24 @@ while True:
     _, frame = cap.read()
 
     cv2.imshow('frame', frame)
+    data_to_analysis_one_asparagus_images = asparaguses_detector.data_to_analysis_one_asparagus_images(frame)
+
+    if not data_to_analysis_one_asparagus_images:
+        continue
+
+    actual_asparaguses_bounding_rectangle = []
+    actual_asparagus_classes = []
+    for data_to_analysis_one_asparagus_image in data_to_analysis_one_asparagus_images:
+        actual_asparaguses_bounding_rectangle.append(data_to_analysis_one_asparagus_image.rectangle_on_original_image)
+        asparagus = one_asparagus_analyzer.asparagus(data_to_analysis_one_asparagus_image.image)
+        actual_asparagus_classes.append(asparagus_classifier.classify(asparagus))
+
+    buckets = buckets_detector.buckets_on_image(frame)
+
+    bucket_asparagus_pairs = MergeBucketsAndAsparagusPositions(buckets, actual_asparaguses_bounding_rectangle, actual_asparagus_classes).bucket_asparagus_pairs
+
+    for bucket_asparagus_pair in bucket_asparagus_pairs:
+        displayer.add_new_result(bucket_asparagus_pair[0], bucket_asparagus_pair[1])
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
