0fff9ad457a843f26fa9eb24eac2dc5611ec3f23
bbaattaahh <bbaattaahh@gmail.com>
2017-05-15 17:06:03 +0200

    parallel first try

---
+++ auxiliary_scripts/width_high/width_high_parallel.py
@@ -0,0 +1,85 @@
+from moviepy.editor import *
+import cv2
+import json
+import numpy as np
+import datetime
+import multiprocessing as mp
+
+from MeasurementsEvaulatorWidthHigh import MeasurementsEvaluatorWidthHigh
+from DisplayClassification import DisplayClassification
+from OneFrameWidthHighProcessor import OneFrameWidthHighProcessor
+
+
+def dummy(processor_1, frame):
+    processor_1.process_frame(frame)
+    return None
+
+
+with open('./auxiliary_scripts/width_high/config_width_high.json') as data_file:
+    config = json.load(data_file)
+
+measurements_evaluator = MeasurementsEvaluatorWidthHigh(
+                                config["measurements_evaluator_width_high"]["minimum_repeated_measurement_number"],
+                                config["measurements_evaluator_width_high"]["no_on_screen_time_before_display"],
+                                config["measurements_evaluator_width_high"]["survive_time"])
+
+processor_1 = OneFrameWidthHighProcessor(config_file='./auxiliary_scripts/width_high/config_width_high.json',
+                                         measurements_evaluator=measurements_evaluator)
+
+processor_2 = OneFrameWidthHighProcessor(config_file='./auxiliary_scripts/width_high/config_width_high.json',
+                                         measurements_evaluator=measurements_evaluator)
+
+
+p = mp.Pool(processes=2)
+
+displayer = DisplayClassification()
+
+clip = VideoFileClip("/Users/h.bata/Videos/acss/two_lamps/Video 8.mp4")
+
+# cap = cv2.VideoCapture(0)
+# cap.set(3, config["web_camera_distribution"][0])
+# cap.set(4, config["web_camera_distribution"][1])
+
+#while True:
+start = datetime.datetime.now()
+
+for x in clip.iter_frames():
+    start_1 = datetime.datetime.now()
+    #_, frame = cap.read()
+
+    frame = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+    frame = np.array(np.rot90(frame, config["rotation_factor"]))
+
+    while not processor_1.is_it_free:
+        print("please stand by")
+
+    p.apply_async(processor_1.process_frame, args=(frame, ))
+    # p.apply_async(print("jani"))
+
+    small = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
+    cv2.imshow('frame', small)
+
+    actual_raw_feed = measurements_evaluator.get_display_feed()
+
+    if actual_raw_feed:
+        # print(actual_raw_feed)
+        width_high_data = str(actual_raw_feed[1]) + "    " + str(actual_raw_feed[2])
+        displayer.add_new_result(actual_raw_feed[0], width_high_data)
+
+    displayer.display_actual()
+
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+    end_1 = datetime.datetime.now()
+    #print("Loop time:")
+    #print(end_1 - start_1)
+
+cv2.destroyAllWindows()
+end = datetime.datetime.now()
+# cap.release()
+
+print(end - start)
+displayer.kill()
---
+++ classes/OneFrameWidthHighProcessor.py
@@ -0,0 +1,84 @@
+import json
+import cv2
+import time
+
+from BucketsDetector import BucketsDetector
+from AsparagusesDetector2 import AsparagusesDetector2
+from OneAsparagusAnalyzer import OneAsparagusAnalyzer
+from MergeBucketsAndAsparagusesPositionsWidthHigh import MergeBucketsAndAsparagusPositionsWidthHigh
+
+
+class OneFrameWidthHighProcessor(object):
+    def __init__(self,
+                 config_file,
+                 measurements_evaluator):
+
+        with open(config_file) as data_file:
+            config = json.load(data_file)
+
+        bucket_marker_template = cv2.imread(config["bucket_detector"]["template"])
+
+        self.buckets_detector = BucketsDetector(bucket_marker_template,
+                                           tuple(
+                                               config["bucket_detector"]["bucket_marker_template_original_resolution"]),
+                                           tuple(config["bucket_detector"]["template_matching_resolution"]),
+                                           config["bucket_detector"]["max_bucket_number"],
+                                           config["bucket_detector"]["expected_template_matching_threshold"],
+                                           config["bucket_detector"]["numbers_folder"],
+                                           config["bucket_detector"]["number_matching_resolution"])
+
+        self.asparaguses_detector = AsparagusesDetector2(config["asparaguses_detector"]["global_threshold"],
+                                                    config["asparaguses_detector"]["high_width_ratio"],
+                                                    config["asparaguses_detector"]["minimum_area"],
+                                                    tuple(config["asparaguses_detector"]["detection_resolution"]),
+                                                    config["asparaguses_detector"]["extension_factor"])
+
+        self.one_asparagus_analyzer = OneAsparagusAnalyzer(asparagus_head_classifier=None)
+        self.mm_pixel_ratio = config["asparagus_classifier"]["millimeter_pixel_ratio"]
+        self.measurements_evaluator = measurements_evaluator
+        self.is_it_free=True
+
+    def process_frame(self, frame):
+        self.is_it_free = False
+
+        time.sleep(10)
+
+        data_to_analysis_one_asparagus_images = self.asparaguses_detector.data_to_analysis_one_asparagus_images(frame)
+
+        actual_width_high = []
+        actual_asparaguses_bounding_rectangle = []
+
+        for data_to_analysis_one_asparagus_image in data_to_analysis_one_asparagus_images:
+
+            asparagus_contour = self.one_asparagus_analyzer.asparagus_contour(data_to_analysis_one_asparagus_image.image)
+
+            raw_thickness = self.one_asparagus_analyzer.asparagus_thickness(asparagus_contour)
+            thickness = self.change_pixel_to_mm(raw_thickness)
+
+            raw_length = self.one_asparagus_analyzer.asparagus_length(asparagus_contour)
+            length = self.change_pixel_to_mm(raw_length)
+
+            actual_asparaguses_bounding_rectangle.append(
+                data_to_analysis_one_asparagus_image.opencv_rectangle_on_original_image)
+            actual_width_high.append([thickness, length])
+
+        buckets = self.buckets_detector.buckets_on_image(frame)
+
+
+        bucket_asparagus_pairs = MergeBucketsAndAsparagusPositionsWidthHigh(buckets,
+                                                                            actual_asparaguses_bounding_rectangle,
+                                                                            actual_width_high).bucket_asparagus_pairs
+
+        if bucket_asparagus_pairs:
+            print(bucket_asparagus_pairs)
+            for bucket_asparagus_pair in bucket_asparagus_pairs:
+                self.measurements_evaluator.add_measurement(bucket_number=bucket_asparagus_pair[0],
+                                                            width=bucket_asparagus_pair[1][0],
+                                                            high=bucket_asparagus_pair[1][1])
+
+        self.is_it_free = True
+
+    def change_pixel_to_mm(self, measurement):
+        measurement = measurement * self.mm_pixel_ratio
+        measurement = round(measurement, 1)
+        return measurement
